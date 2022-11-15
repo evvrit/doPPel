f34002b9670d0387aaf4b5b97679f7a72297f197
Elyse Turpin <turpin.elyse@gmail.com>
2022-11-15 11:24:06 -0500

    doppelganger controller made

---
+++ app/controllers/doppelgangers_controller.rb
@@ -0,0 +1,30 @@
+class DoppelgangersController < ApplicationController
+
+  def index
+    @doppelgangers = Doppelganger.all
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+end
