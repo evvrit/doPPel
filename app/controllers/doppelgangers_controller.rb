c85147d730c5976e5a5c6ffce629b49fdfecbbe4
Elyse Turpin <turpin.elyse@gmail.com>
2022-11-15 11:58:48 -0500

    doppelganger actions index, show, new, edit are made, and a template for each

--- app/controllers/doppelgangers_controller.rb
+++ app/controllers/doppelgangers_controller.rb
@@ -1,30 +1,37 @@
 class DoppelgangersController < ApplicationController
+  before_action :set_doppelganger, only: %i[show edit update destroy]
 
   def index
     @doppelgangers = Doppelganger.all
   end
 
   def show
-
   end
 
   def new
-
+    @doppelganger = Doppelganger.new
   end
 
   def create
-
+    raise
   end
 
   def edit
-
   end
 
   def update
-
   end
 
   def destroy
+  end
 
+  private
+
+  def doppelganger_params
+
+  end
+
+  def set_doppelganger
+    @doppelganger = Doppelganger.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+
+  get "/agent-dopplegangers/:id", to: "doppelgangers#show"
+  resources :doppelgangers
 end
