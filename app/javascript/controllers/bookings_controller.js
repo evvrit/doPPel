6c2716ef8358bf944b5fc7ed78396b60d7a34aa6
Elyse Turpin <turpin.elyse@gmail.com>
2022-11-16 14:31:53 -0500

    booking show page has conditioning routing to set booking status

--- app/javascript/controllers/bookings_controller.js
+++ app/javascript/controllers/bookings_controller.js
@@ -2,17 +2,22 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="bookings"
 export default class extends Controller {
-  static targets = ["acceptButton", "rejectButton", "cancelButton"]
+  static targets = ["acceptButton", "rejectButton", "cancelButton"];
+
+  connect() {
+    console.log(this.element)
+  }
 
   accept() {
-    console.log(this.acceptButtonTarget)
+    console.log(this.element)
+    console.log(this.acceptButtonTarget);
   }
 
   reject(event) {
-    console.log(event)
+    console.log(event);
   }
 
   cancel(event) {
-    console.log(event)
+    console.log(event);
   }
 }
