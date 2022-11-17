df76337c9cfa4d8c9a7a1627e8c46577dccec978
Elyse Turpin <turpin.elyse@gmail.com>
2022-11-17 16:55:04 -0500

    search bar for address input

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,6 +4,9 @@
 
 import { application } from "./application"
 
+import AddressAutocompleteController from "./address_autocomplete_controller"
+application.register("address-autocomplete", AddressAutocompleteController)
+
 import BookingsController from "./bookings_controller"
 application.register("bookings", BookingsController)
 
