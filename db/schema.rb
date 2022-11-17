9c5ddb7dc3b2a49d9b0beb6a892f424ceebce105
Vinh Tho Hoang <vinhtho.hoang@gmail.com>
2022-11-17 14:19:16 -0500

    pull request for elyse

--- db/schema.rb
+++ db/schema.rb
@@ -69,7 +69,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_17_034341) do
     t.float "longitude"
     t.integer "gender", default: 0
     t.string "address"
-    t.integer "gender", default: 0
     t.index ["user_id"], name: "index_doppelgangers_on_user_id"
   end
 
