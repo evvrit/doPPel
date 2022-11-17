db59118e678d8a92ed5f135acd78665630e64ba0
Elyse Turpin <turpin.elyse@gmail.com>
2022-11-17 11:35:18 -0500

    booking form problem resolved

--- app/models/doppelganger.rb
+++ app/models/doppelganger.rb
@@ -9,22 +9,20 @@ class Doppelganger < ApplicationRecord
   validates :name, presence: true
   validates :address, presence: true
   validates :age, presence: true, comparison: { greater_than_or_equal_to: 21 }
-  # validates :location, presence: true, inclusion: { in: %w[Montreal Toronto Ottawa] }
   validates :rate, presence: true, comparison: { greater_than_or_equal_to: 20 }
-  # validates :bio, presence: true, length: { in: 10..280 }
   validates :gender, presence: true
+  validates :bio, presence: true, length: { in: 10..280 }
   enum gender: {
     other: 0,
     male: 1,
     female: 2,
     nonbinary: 3
   }
-  validates :bio, presence: true, length: { in: 10..280 }
 
-  include PgSearch::Model
-  pg_search_scope :search_by_name_and_address_and_bio,
-    against: [ :name, :address, :bio ],
-    using: {
-      tsearch: { prefix: true }
-    }
+  # include PgSearch::Model
+  # pg_search_scope :search_by_name_and_address_and_bio,
+  #   against: [ :name, :address, :bio ],
+  #   using: {
+  #     tsearch: { prefix: true }
+  #   }
 end
