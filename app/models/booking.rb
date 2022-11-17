8f93f221f5a5bc7ff3c0d775fc02f8e684222489
Elyse Turpin <turpin.elyse@gmail.com>
2022-11-17 16:22:17 -0500

    can only add booking date condition to make sure it doesn't start in the past

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,10 +1,12 @@
 class Booking < ApplicationRecord
   belongs_to :doppelganger
   belongs_to :user
-  validates :start_date, presence: true, comparison: { greater_than: DateTime.now + 0.02 } # <- that's 30 mins from moment of booking
-  validates :end_date, presence: true, comparison: { greater_than: :start_date }
+
+  validates :start_date, presence: true, comparison: { greater_than_or_equal_to: Time.now } # <- that's 30 mins from moment of booking
+  validates :end_date, presence: true, comparison: { greater_than: :start_date  }
   validates :address, presence: true
   validates :status, presence: true
+
   enum status: {
     pending: 0,
     accepted: 1,
