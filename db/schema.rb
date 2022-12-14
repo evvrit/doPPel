03821f7d5932de2368728c35ce09351228e32d4f
Richard Young <richarddfyoung@gmail.com>
2022-12-14 18:01:53 -0500

    redid CSS again because it was wrong size. made thigs flexy. added seed photos to doppelgangers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_17_034341) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_14_225741) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -69,6 +69,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_17_034341) do
     t.float "latitude"
     t.float "longitude"
     t.string "address"
+    t.string "seed_photo"
     t.index ["user_id"], name: "index_doppelgangers_on_user_id"
   end
 
