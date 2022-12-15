4be1894f4bbe09e75485edc0a8fa555604a67e8a
Richard Young <richarddfyoung@gmail.com>
2022-12-14 19:04:00 -0500

    tweaked seeds to include seed_photos for doppelgangers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,35 @@
 require "faker"
 
+photos = [
+  "beret.avif",
+  "black_hoodie.avif",
+  "coiffed.avif",
+  "crazy_hair.avif",
+  "grey_beard.avif",
+  "hacker.avif",
+  "nose_ring.avif",
+  "shawl_neck.avif",
+  "white_teeth.avif",
+  "blonde_profile.avif",
+  "blue_tint.avif",
+  "business.avif",
+  "cool_hair.avif",
+  "curly_hair.avif",
+  "denim.avif",
+  "earrings.avif",
+  "green_hair.avif",
+  "horizontal_stripes.avif",
+  "pale_profile.avif",
+  "red_sweater.avif",
+  "striped_shirt.avif"
+]
+
 puts "cleaning the DB of users and doppelgangers..."
 Doppelganger.destroy_all
 User.destroy_all
 puts "DB is clean!"
 
-puts "making 24 fake doppelgangers..."
+puts "making static account Tommy Jensen email: a@a.a password: secret"
 
 User.create(
   email: "a@a.a",
@@ -14,12 +38,16 @@ User.create(
   last_name: "Jensen"
 )
 
+puts "Static account made"
+puts "Making 24 random users"
+
 24.times do User.create(
   email: "#{Faker::Games::Pokemon.name}@doppel.com",
   password: "secret",
   first_name: Faker::Name.first_name,
   last_name: Faker::Name.last_name
 )
+  puts "made user #{User.last.first_name} #{User.last.last_name}"
 end
 
 # def build_address
@@ -29,6 +57,8 @@ end
 #   return num.to_s + " " + street + ", " + city
 # end
 
+puts "making 24 doppelgangers, one for each user"
+
 User.all.each do |user|
   Doppelganger.create(
     name: "#{user.first_name} #{user.last_name}",
@@ -37,8 +67,10 @@ User.all.each do |user|
     address: Faker::Address.full_address,
     rate: rand(20..500),
     bio: Faker::Movies::HitchhikersGuideToTheGalaxy.marvin_quote,
-    user: user
+    user: user,
+    seed_photo: photos.sample
   )
+  puts "made doppelganger for #{user.first_name}"
 end
 
 # 20.times do Booking.new(
